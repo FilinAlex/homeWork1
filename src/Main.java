2eec4c5f7fb96232a18763b344a2ad4654baf1c8
FilinAlex <filin.oleksander@gmail.com>
2021-10-08 23:22:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+// 10*9*8
+        factorial(10);
+    }
+
+    public static int factorial(int a) {
+           if (a == 1){
+                 return 1;
+             }
+        int result = a * factorial(a-1);
+        System.out.println(result);
+           return result;
+    }
+
+}
